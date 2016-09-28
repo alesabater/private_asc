d4d9a5e71ceb6ad2d8e2a738aa20a3cd14d94a9b
BATANERO <U553574@ads.dlh.de>
2016-09-28 11:16:18 +0200

    HADOOP-282: Move test cases to DataFrameCommonTest
    Two test cases have been moved from TransformationsTests to
    DataFrameCommonTest

--- src/test/scala/de/dlh/smile/axdelivery/DestinationModel/DataFrameCommonsTest.scala
+++ src/test/scala/de/dlh/smile/axdelivery/DestinationModel/DataFrameCommonsTest.scala
@@ -1,5 +1,6 @@
 package de.dlh.smile.axdelivery.DestinationModel
 
+import de.dlh.smile.engine.commons.{Contexts, LoadedProperties}
 import de.dlh.smile.axdelivery.Stub
 import org.scalatest.{FlatSpec, Matchers}
 import de.dlh.smile.axdelivery.DestinationModel.DataFrameCommons._
@@ -8,13 +9,25 @@ import org.apache.spark.sql.functions._
 
 class DataFrameCommonsTest extends FlatSpec with Matchers {
 
-  "getBFTUDEPField" should "create a column with the BFTUDE column" in {
-    val df = Stub.dfMapDate
-    val dfResult = df.getBFTUDEPField("date_prev","date_dep","k1")
-    dfResult.show
-    dfResult.printSchema()
-    dfResult.filter(col("BFTUDEP").isNotNull).count should equal(4)
-    dfResult.select("BFTUDEP").take(4) should equal(Array(Row(12),Row(11),Row(-4),Row(620)))
-  }
+	"getBFTUDEPField" should "create a column with the BFTUDEP column" in {
+		val df = Stub.dfMapDate
+				val dfResult = df.getBFTUDEPField("date_prev","date_dep","k1")
+				//dfResult.show
+				//dfResult.printSchema()
+				dfResult.filter(col("BFTUDEP").isNotNull).count should equal(4)
+				dfResult.select("BFTUDEP").take(4) should equal(Array(Row(12),Row(11),Row(-4),Row(620)))
+	}
 
+	"filterByDate" should "filter dataframe based on dates" in {
+		val df = Stub.dfFilterDate
+				val dfResult = df.filterPartitionFieldsOneYearFrom(2016, 9)
+				dfResult.count() should equal(2)
+	}
+
+	"filterFromMap" should "filter dataframe based on a Map column key and value" in {
+		val df = Contexts.sqlCtx.read.parquet(getClass.getResource("/data").getPath)
+		    df.count()
+				df.filterValueMapEquals("cs_uri_query", "Screen", "FOFP")
+				df.count()
+	}
 }
